b76f86857fda8228b2cf0c37becaff05599243fd
Sizzlesina <www.sins604@gmail.com>
2023-08-13 12:24:25 +0330

    added the jonas html code and finished the next video then we have a code challenge but i might take some rest so...

--- A-closer-look-at-functions/script.js
+++ A-closer-look-at-functions/script.js
@@ -224,7 +224,7 @@ function count () {
 // // in the CALL METHOD we will tell to the variable that which function or object we gonna use and give it the values to use the this keyword in that function
 // book.call(eurowings,23,"Sarah Williams");
 // console.log(eurowings);
-// // now in here we can use the this keyword for each of the objects 
+// // now in here we can use the this keyword for each of the objects
 
 // book.call(lufthansa,239,"Mary Copper");
 // console.log(lufthansa);
@@ -250,3 +250,78 @@ function count () {
 
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------]
 
+// << The Bind Method >>
+
+"use strict";
+const lufthansa = {
+  airline: "Lufthansa",
+  iataCode: "LH",
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+const eurowings = {
+  airline: "Eurowings",
+  iataCode: "EW",
+  bookings: [],
+};
+const swiss = {
+  airline: "Swiss Air Lines",
+  iataCode: "LX",
+  bookings: [],
+};
+const book = lufthansa.book;
+// so in here we will just set the this keyword into an variable
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+// now in here using the this keyword of the variable we will be able to set a value for the function
+bookEW(23, "Steven Williams");
+
+// now we keep the "23" and reserve two 23 flight for diffrent passengers
+// in the bind method the argument will be set in stoned
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23("Jonas Schemedtmann");
+bookEW23("Martha Cooper");
+
+// With Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+document.querySelector(".buy").addEventListener("click", lufthansa.buyPlane);
+// now these line of codes will return a NaN and that's because the this keyword will point to the button
+// here's the solution:
+document
+  .querySelector(".buy")
+  .addEventListener("click", lufthansa.buyPlane.bind(lufthansa));
+
+// Partial Application
+  const addTax = (rate,value) => value + value * rate;
+  console.log(addTax(0.1,200));
+
+  const addVAT = addTax.bind(null,0.25); // now in this line of code we set the rate in stoned
+  // addVAT = value => value + value * 0.25
+
+
+  console.log(addVAT(100));
+  console.log(addVAT(23));
+  
+  const addTaxRate = function(rate){
+    return function(value){
+      return value + value * rate;
+    }
+  }
+  const addVAT2 = addTaxRate(0.23);
+  console.log(100);
+  console.log(23);
+// im sleepy man might just go take some rest
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------]
