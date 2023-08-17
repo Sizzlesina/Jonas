621b18f0d6c51712b5be3426a6b9160af472fa0e
Sizzlesina <www.sins604@gmail.com>
2023-08-17 09:14:42 +0330

    finished the functions section

--- A-closer-look-at-functions/script.js
+++ A-closer-look-at-functions/script.js
@@ -431,15 +431,14 @@ function count () {
 // Lets build a simple poll app!
 
 // A poll has a question, an array of options from which
-// people can choose, and an array with the number of 
-// replies for each option. This data is stored in the 
+// people can choose, and an array with the number of
+// replies for each option. This data is stored in the
 // starter object below.
 
-
 // Here are your tasks:
-// 1. Create a method called 'registerNewAnswer' on the 
+// 1. Create a method called 'registerNewAnswer' on the
 // 'poll' object. The method does 2 things:
-// 1.1. Display a prompt window for the user to input 
+// 1.1. Display a prompt window for the user to input
 // the number of the selected option. The propmpt
 // should look like this:
 //   What is your favourite programming language?
@@ -449,20 +448,19 @@ function count () {
 //   3: C++
 //   (Write option number)
 
-
 //   1.2. Based on the input number, update the answers
 //   array, For example if the option is 3, Increase
 //   the value AT POSITION 3 of the array by 1. Make
 //   sure to check if the input is a number and if the
-//   number makes sense (e.g answer 52 wouldn't make 
+//   number makes sense (e.g answer 52 wouldn't make
 //   sense right?)
 
 // 2. Call this method whenever the user clicks the "Answer poll" button
 
 // 3. Create a method 'displayResults' which displays
-// the poll results. The method takes a string as an 
+// the poll results. The method takes a string as an
 // input (called 'type'), which can be either 'string'
-// or 'array'. If type is 'array', simply display the 
+// or 'array'. If type is 'array', simply display the
 // results array as it is, using console.log(). This
 // should be the default option. If type is 'string'
 // display a string like 'Poll results are 13, 2, 4 ,1'.
@@ -470,7 +468,7 @@ function count () {
 // 4. Run the 'displayResults' method at the end of each
 // 'registerNewAnswer' method call.
 
-// HINT: Use many of the tools you learned about in this 
+// HINT: Use many of the tools you learned about in this
 // and the last lecture
 
 // BONUS: Use the 'displayResults' method to display the
@@ -495,7 +493,7 @@ function count () {
 //     typeof answer === "number" &&
 //       answer < this.answers.length &&
 //       this.answers[answer]++;
-      
+
 //       this.displayResults();
 //       this.displayResults('string');
 //   },
@@ -551,5 +549,85 @@ function count () {
 // counter();
 
 // console.dir(counter); // this line of code is simmilar to console.log
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------]
+
+// // << Closures pt 2 >>
+
+// // we want to have two more examples of the closures
+
+// "use strict";
+
+// // EXAMPLE 1
+// let closure;
+
+// const fun = function () {
+//   const num = 23;
+//   closure = function () {
+//     console.log(num * 2);
+//   };
+// };
+
+// fun(); // this function will keep the variable declations inside of itself
+// closure(); // and this function will do the process
+// console.dir(closure); // we can see the directory of the closure like this
+
+// // now we want to assign another function and see if the closure works
+// // Re-assgning closure function
+// const anotherFun = function (){
+//   const num1 = 777;
+//   closure = function () {
+//     console.log(num1 * 2);
+//   };
+// }
+
+// anotherFun();
+// closure(); // as we can see it works
+// console.dir(closure);
+
+// // EXAMPLE 2
+
+// const boardPassengers = function(n,wait){
+//   const perGroup = n / 3;
+
+//   setTimeout(function(){
+//     console.log(`We are now boarding all ${n} passengers`);
+//     console.log(`There are 3 groups, each with ${perGroup} passengers`);
+//   }, wait * 1000);
+
+//   console.log(`Will start boarding in ${wait} seconds`);
+// }
+
+// boardPassengers(180,3);
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------]
+
+// // << Coding Challenge #2 >>
+
+// /* 
+// this is more of a thinking challenge than a coding 
+// challenge 🥸
+
+// Take the IIFE below and at the end of the function,
+// attach an event listener that changes the color of
+// the selected h1 element ('header') to blue, each time
+// the BODY element is clicked, DO NOT select the h1
+// element again!
+
+// And now explain to YOURSELF (or someone around you)
+// WHY this worked! Take all the time you need, Think
+// about WHEN exactly the callback function is executed,
+// and what that means for the variables involved in
+// this example.
+
+// GOOD LUCK 😉
+// */
+// (function(){
+//   const header = document.querySelector('h1');
+//   header.style.color = 'red';
+//   document.querySelector('body').addEventListener('click',function(){
+//     header.style.color = 'blue';
+//   })
+// })();
 
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------]
