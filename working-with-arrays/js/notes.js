b07a541c6ccfe51e79c3a24d41d032206a2173d7
Sizzlesina <www.sins604@gmail.com>
2023-08-20 13:25:30 +0330

    coding challenge 3 done;

--- working-with-arrays/js/notes.js
+++ working-with-arrays/js/notes.js
@@ -264,6 +264,27 @@ calcDisplayBalance(account1.movements);
 
 // << Part 4 >>
 
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter((mov) => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+  const outcomes = movements
+    .filter((mov) => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(outcomes)}€`;
+  const interest = movements
+    .filter((mov) => mov > 0)
+    .map((deposit) => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
+calcDisplaySummary(account1.movements);
+
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // // << Coding Challenge #1 >>
@@ -424,29 +445,87 @@ calcDisplayBalance(account1.movements);
 
 // // Maximum value
 // const max = movements.reduce(function(acc,mov){
-//   if(acc > mov) 
+//   if(acc > mov)
 //   return acc;
-// else 
+// else
 //   return mov;
 // },movements[0])
 // console.log(max);
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-// Coding Challenge #2
+// // Coding Challenge #2
 
-/* 
-Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+// /*
+// Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
 
-Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+// Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
 
-1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
-2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
-3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
-4. Run the function for both test datasets
+// 1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+// 2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+// 3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+// 4. Run the function for both test datasets
 
-TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
-TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+// TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+// TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 
-GOOD LUCK 😀
-*/
+// GOOD LUCK 😀
+// */
+
+// const calcAverageHumanAge = (ages) => {
+//   const humanAges = ages.map((age) => (age <= 2 ? 2 * age : 16 + age * 4));
+//   console.log(humanAges);
+//   const adults = humanAges.filter((age) => age <= 18);
+//   console.log(humanAges);
+//   console.log(adults);
+
+//   const average = adults.reduce(
+//     (acc, age, i, arr) => acc + age / arr.length,
+//     0
+//   );
+
+//   return average;
+// };
+
+// const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+// const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+// console.log(avg1, avg2);
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// // << The Magic of Chaining Methods >>
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const euroToUsd = 1.1;
+// // PIPELINE
+// const totalDepositsUSD = movements
+//   .filter((mov) => mov > 0)
+//   .map((mov) => Math.trunc(mov * euroToUsd))
+//   .reduce((acc, mov) => acc + mov, 0);
+// console.log(totalDepositsUSD);
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// // Coding Challenge #3
+
+// /* 
+// Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
+
+// TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+// TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+// GOOD LUCK 😀
+// */
+
+// const calcAverageHumanAge = (ages) => {
+//   const humans = ages
+//     .map((age) => (age <= 2 ? 2 * age : 16 + age * 4))
+//     .filter((age) => age <= 18)
+//     .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+//     return humans;
+//   };
+// const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+// const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+// console.log(avg1,avg2);
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
