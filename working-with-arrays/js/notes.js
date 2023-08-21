0bf58b681fd48aa538c2e55438fb5a1fd2ff6f36
Sizzlesina <www.sins604@gmail.com>
2023-08-21 12:15:07 +0330

    fix the script codes

--- working-with-arrays/js/notes.js
+++ working-with-arrays/js/notes.js
@@ -1,5 +1,178 @@
 /** @format */
 
+
+// << PROJECT _Bankist_App >>
+// Part 1
+
+// Account Declarations
+const account1 = {
+  owner: "Jonas Schmedtmann",
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: "Jessica Davis",
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: "Steven Thomas Williams",
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: "Sarah Smith",
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+// Element Declarations
+
+// Label Elements
+const labelWelcome = document.querySelector(".welcome");
+const labelDate = document.querySelector(".date");
+const labelBalance = document.querySelector(".balance__value");
+const labelSumIn = document.querySelector(".summary__value--in");
+const labelSumOut = document.querySelector(".summary__value--out");
+const labelSumInterest = document.querySelector(".summary__value--interest");
+const labelTimer = document.querySelector(".timer");
+
+// Container Elements
+const containerApp = document.querySelector(".app");
+const containerMovements = document.querySelector(".movements");
+
+// Btn Elements
+const btnLogin = document.querySelector(".login__btn");
+const btnTransfer = document.querySelector(".form__btn--transfer");
+const btnLoan = document.querySelector(".form__btn--loan");
+const btnClose = document.querySelector(".form__btn--close");
+const btnSort = document.querySelector(".btn--sort");
+
+// Input Elements
+const inputLoginUsername = document.querySelector(".login__input--user");
+const inputLoginPin = document.querySelector(".login__input--pin");
+const inputTransferTo = document.querySelector(".form__input--to");
+const inputTransferAmount = document.querySelector(".form__input--amount");
+const inputLoanAmount = document.querySelector(".form__input--loan-amount");
+const inputCloseUsername = document.querySelector(".form__input--user");
+const inputClosePin = document.querySelector(".form__input--pin");
+
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = ""; // remove the last movements and replace the new ones
+
+  movements.forEach(function (mov, i) {
+    // in the function inside the forEach loop the two parameters are return the value of the array and the index of it
+    const type = mov > 0 ? "deposit" : "withdrawal";
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${mov}€</div>
+      </div>
+    `;
+
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+    // the insertAdJacentHTML can return two values as "afterbegin" and "beforeend" and in here we use the first one because if we were use the second one it will return
+    // the value of the array from end to first
+  });
+};
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// part 2
+
+// Computing Usernames >>
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(" ")
+      .map((name) => name[0])
+      .join("");
+  });
+};
+createUsernames(accounts);
+console.log(accounts);
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// << Part 3 >>
+// << Adding a calcPrint function using reduce method >>
+
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// << Part 4 >>
+// << Adding a calcDisplaySummary function with chaining methods >>
+
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
+    .filter((mov) => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+  const outcomes = acc.movements
+    .filter((mov) => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(outcomes)}€`;
+  const interest = acc.movements
+    .filter((mov) => mov > 0)
+    .map((deposit) => (deposit * acc.interestRate) / 100)
+    .filter((int, i, arr) => {
+      console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Part 5
+// << Implementing Login >>
+
+// Event handler
+let currentAccount;
+btnLogin.addEventListener("click", function (e) {
+  // prevent form from submitting
+  e.preventDefault();
+
+  currentAccount = accounts.find(
+    (acc) => acc.username === inputLoginUsername.value
+  ); // question : where is the userName in the object? well we created a funciton that creates a userName earlier and then add it to the object
+  console.log(currentAccount);
+  if (currentAccount.pin === Number(inputLoginPin.value)) {
+    // Display UI and message
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(" ")[0]
+    }.`;
+    containerApp.style.opacity = 100;
+    // Clear input fields
+    inputLoginUsername.value = inputLoginPin.value = "";
+    // Display movements
+    displayMovements(currentAccount.movements);
+    // Display balance
+    calcDisplayBalance(currentAccount.movements);
+    // Display summary
+    calcDisplaySummary(currentAccount);
+  } else alert("Wrong Password!");
+});
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 // // << working with arrays >>
 // const current = new Map([
 //   ['USD','United States dollar'],
@@ -143,150 +316,6 @@
 // // if we use '_' in the parameters of a function it will mean that this is a throwaway
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-// << PROJECT _Bankist_App >>
-// Part 1
-
-// Account Declarations
-const account1 = {
-  owner: "Jonas Schmedtmann",
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
-};
-
-const account2 = {
-  owner: "Jessica Davis",
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
-};
-
-const account3 = {
-  owner: "Steven Thomas Williams",
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: "Sarah Smith",
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
-// Element Declarations
-
-// Label Elements
-const labelWelcome = document.querySelector(".welcome");
-const labelDate = document.querySelector(".date");
-const labelBalance = document.querySelector(".balance__value");
-const labelSumIn = document.querySelector(".summary__value--in");
-const labelSumOut = document.querySelector(".summary__value--out");
-const labelSumInterest = document.querySelector(".summary__value--interest");
-const labelTimer = document.querySelector(".timer");
-
-// Container Elements
-const containerApp = document.querySelector(".app");
-const containerMovements = document.querySelector(".movements");
-
-// Btn Elements
-const btnLogin = document.querySelector(".login__btn");
-const btnTransfer = document.querySelector(".form__btn--transfer");
-const btnLoan = document.querySelector(".form__btn--loan");
-const btnClose = document.querySelector(".form__btn--close");
-const btnSort = document.querySelector(".btn--sort");
-
-// Input Elements
-const inputLoginUsername = document.querySelector(".login__input--user");
-const inputLoginPin = document.querySelector(".login__input--pin");
-const inputTransferTo = document.querySelector(".form__input--to");
-const inputTransferAmount = document.querySelector(".form__input--amount");
-const inputLoanAmount = document.querySelector(".form__input--loan-amount");
-const inputCloseUsername = document.querySelector(".form__input--user");
-const inputClosePin = document.querySelector(".form__input--pin");
-
-const displayMovements = function (movements) {
-  containerMovements.innerHTML = ""; // remove the last movements and replace the new ones
-
-  movements.forEach(function (mov, i) {
-    // in the function inside the forEach loop the two parameters are return the value of the array and the index of it
-    const type = mov > 0 ? "deposit" : "withdrawal";
-    const html = `
-      <div class="movements__row">
-        <div class="movements__type movements__type--${type}">${
-      i + 1
-    } ${type}</div>
-        <div class="movements__value">${mov}€</div>
-      </div>
-    `;
-
-    containerMovements.insertAdjacentHTML("afterbegin", html);
-    // the insertAdJacentHTML can return two values as "afterbegin" and "beforeend" and in here we use the first one because if we were use the second one it will return
-    // the value of the array from end to first
-  });
-};
-
-displayMovements(account1.movements);
-
-// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-// part 2
-
-// Computing Usernames >>
-
-const createUsernames = function (accs) {
-  accs.forEach(function (acc) {
-    acc.username = acc.owner
-      .toLowerCase()
-      .split(" ")
-      .map((name) => name[0])
-      .join("");
-  });
-};
-createUsernames(accounts);
-console.log(accounts);
-
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-// << Part 3 >>
-// << Adding a calcPrint function using reduce method >>
-
-const calcDisplayBalance = function (movements) {
-  const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance} EUR`;
-};
-calcDisplayBalance(account1.movements);
-
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-// << Part 4 >>
-
-const calcDisplaySummary = function (movements) {
-  const incomes = movements
-    .filter((mov) => mov > 0)
-    .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
-  const outcomes = movements
-    .filter((mov) => mov < 0)
-    .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(outcomes)}€`;
-  const interest = movements
-    .filter((mov) => mov > 0)
-    .map((deposit) => (deposit * 1.2) / 100)
-    .filter((int, i, arr) => {
-      console.log(arr);
-      return int >= 1;
-    })
-    .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
-};
-calcDisplaySummary(account1.movements);
-
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
 // // << Coding Challenge #1 >>
 
 // /*
@@ -508,7 +537,7 @@ calcDisplaySummary(account1.movements);
 
 // // Coding Challenge #3
 
-// /* 
+// /*
 // Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
 
 // TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
@@ -527,5 +556,32 @@ calcDisplaySummary(account1.movements);
 // const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 // const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 // console.log(avg1,avg2);
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// // << The Find Method >>
+
+// // FIND => will accept callback function and condition
+
+// // this method will not return a new array but it will only returns the first element in the array that satisfies the condition
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const firstWithdrawal = movements.find((mov) => mov < 0);
+// console.log(movements);
+// console.log(firstWithdrawal);
+
+// console.log(accounts);
+
+// const account = accounts.find((acc) => acc.owner === "Jessica Davis");
+// console.log(account);
+
+// for (const acc of accounts) {
+//   if (acc.owner === "Jessica Davis") console.log(acc);
+// }
+
+// // some diffrences between filter and find =>
+// // 1. FIND method will return an element but the FILTER method will return a new array
+
+// // 2. FIND method will only returns the first element that match the condition but the FILTER method will return all the elements which math the condtion
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
