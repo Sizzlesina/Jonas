cc7a6ad884d56d5c7ef41d8f5b43d84e98b4f14b
Sizzlesina <www.sins604@gmail.com>
2023-08-26 17:56:30 +0330

    added the object orineted screen shots to the pic folder and add some descriptions

--- object-oriented-programming/js/notes.js
+++ object-oriented-programming/js/notes.js
@@ -1,1 +1,40 @@
 //      ___________________________________________ OBJECT ORIENTED PROGRAMMING ________________________________________
+
+
+// some descriptions:
+
+
+// << WHAT IS OBJECT ORIENTED PROGRAMMING? >>
+
+// OBJECT_ORIENTED_PROGRAMMING => is a programming paradigm based on the concept of objects
+
+// We use objects to MODEL (Describe) real-world or abstract features;
+
+// Objects may contain data (properties) and code (methods). By using objects, we pack DATA AND THE CORRESPONDING BEHAVIOUR into one block.
+
+// In OOP, Objects are SELF_CONTAINED pieces/blocks of code;
+
+// Objects are BUILDING BLOCKS of applications, and INTERACT with one another.
+
+// Interactions happen through a PUBLIC INTERFACE (API): mehtods that the code OUTSIDE of the object can access and use to communicate with the object.
+
+// OOP was developed with the goal of ORGANIZATION code to make it MORE FLEXIBLE AND EASIER TO MAINTAIN (avoid "spaghetti code")
+
+// << CLASSES AND INSTANCES (Traditional OOP)
+
+// Abstraction => Ignoring or hiding details that DONT MATTER, allowing us to get an OVERVIEW perspective of the thing we're implementing, instead of messing with details that don't really matter to our implementation
+
+// Encapsulation => Keeping the properties and methods PRIVATE inside the class, so they are NOT ACCESSIBLE FROM OUTSIDE THE CLASS. Some methods can be EXPOSED as a public interface (API)
+
+// Inheritance => Making all properties and methods of a certain class AVAILABLE TO A CHILD CLASS, forming a hierachial relationship between classes. This allows us to REUSE COMMON LOGIC and to model real-world relationships.
+
+// Polymorphism => A child class can OVERWRITE a method it inherited from a parent class [it's more complex than that, but enough for out purpose].
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// << OOP in JavaScript >>
+
+// Some descriptions will be added to the box files with the same title name
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
