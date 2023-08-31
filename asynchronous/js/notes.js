7433ac3ad3594280ebaee59491dade955d36ed96
Sizzlesina <www.sins604@gmail.com>
2023-08-31 10:24:44 +0330

    watched two of the videos today

--- asynchronous/js/notes.js
+++ asynchronous/js/notes.js
@@ -54,6 +54,136 @@
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-// << Our first AJAX call XMLHttpRequest >>
+// // << Our first AJAX call XMLHttpRequest >>
+
+// const btn = document.querySelector(".btn-country");
+// const countriesContainer = document.querySelector('.countries')
+
+// const getCountry = function(country){
+// const request = new XMLHttpRequest();
+// request.open("GET",`https://restcountries.com/v2/name/${country}`);
+// request.send();
+
+// console.log(request.responseText);
+
+// request.addEventListener("load",function(){
+//   const [data] = JSON.parse([this.responseText]);
+//   console.log(data);
+//   const html = `
+//   <article class="country">
+//           <img class="country__img" src="${data.flag}" />
+//           <div class="country__data">
+//             <h3 class="country__name">${data.name}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
+//             <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//             <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//           </div>
+//         </article>
+//   `
+//   countriesContainer.insertAdjacentHTML("beforeend",html);
+//   countriesContainer.style.opacity = 1;
+// });
+// }
+// getCountry('iran');
+// getCountry("usa");
+// getCountry("portugal");
+// getCountry("russia");
+
+
+
+
+
+
+// // HINT :
+// //--------------------------------------------------
+// // when we get data from a site that always an backend developer will send the data to us we can manage the data like this 
+// // so the steps are:
+// // 1. first we will get the data in a JSON format
+// // 2. then we convert the data to a javascript format and then do the rhings that we want on the data and write our code 
+// // it that simple and the data will be given by a backend developer so we just only need a frontend UI for the data
+// // for example we can have an object with an empty value and the values of the object will be given to us by the backend developer
+// // alright i think the description was too long so lets go to continue the course
+// //---------------------------------------------------
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// << Welcome to callback hell>>
+
 
 'use strict';
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector('.countries')
+
+
+// this function were added to make our code cleaner
+const renderCountry = function(data,className = ''){
+  const html = `
+  <article class="country ${className}">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+          </div>
+        </article>`
+        countriesContainer.insertAdjacentHTML("beforeend",html);
+        countriesContainer.style.opacity = 1;
+}
+
+const getCountryAndNeighbours = function(country){
+  // AJAX call country 1
+  const request = new XMLHttpRequest;
+  request.open("GET",`https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  console.log(request.responseText);
+
+  request.addEventListener("load",function(){
+    const [data] = JSON.parse([this.responseText])
+    console.log(data);
+    // Render Country 1
+    renderCountry(data);
+
+    // Get neighbour country 2 
+    const neighbour = data.borders[0];
+
+     if(!neighbour) return;
+
+     // AJAX call country 2
+     const request2 = new XMLHttpRequest;
+     request2.open("GET",`https://restcountries.com/v2/alpha/${neighbour}`)
+     request2.send();
+
+     request2.addEventListener('load',function(){
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2,'neighbour') // the class that we added in here was written before ny the jonas
+
+      // here we have a callback function inside an another one that is called the nested function AND THIS IS CALLED "CALLLBACK HELL";
+     })
+  })
+}
+getCountryAndNeighbours("portugal");
+getCountryAndNeighbours("usa");
+
+// an exmaple for callback hell
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 seconds passed');
+    setTimeout(() => {
+      console.log('3 seconds passed');
+    },1000)
+  }, 1000);
+},1000)
+// this function thay have a lot of nested functions is called callback hell
+
+// is callback hell a good thing ? NO
+// so whats the solution? IN THE NEXT VIDEO
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
