428b93413c793a886089b90baff7944ef27d7dc9
Sizzlesina <www.sins604@gmail.com>
2023-09-03 13:48:08 +0330

    added the codes and the files to the repository and start the last lecture of the jonas course

---
+++ moduls-tooling-and-functional/notes/notes.js
@@ -0,0 +1,6 @@
+//      ______________________________________________  Modern JavaScript Development_ Modules, Tooling, and Functional ______________________________________________ 
+
+// << An overview of modern javascript development >>
+
+
+'use strict';
