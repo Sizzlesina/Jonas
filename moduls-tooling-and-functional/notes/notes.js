764e1ba101dfaf47a174a30247b264962375b7dc
Sina <111612123+Sizzlesina@users.noreply.github.com>
2023-09-04 17:50:09 +0330

    Delete moduls-tooling-and-functional directory

--- moduls-tooling-and-functional/notes/notes.js
+++
@@ -1,6 +0,0 @@
-//      ______________________________________________  Modern JavaScript Development_ Modules, Tooling, and Functional ______________________________________________ 
-
-// << An overview of modern javascript development >>
-
-
-'use strict';
